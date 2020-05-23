528c17ad516d2684c64d0b6275283f9f61a22bad
kuznet.i.sova <kis7425910@gmail.com>
2020-05-23 19:35:26 +0300

    fix constraints

--- DJColorViewApp/ViewController.swift
+++ DJColorViewApp/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var djColorView: UIView!
+    
+    @IBOutlet var valueRedSwitch: UILabel!
+    @IBOutlet var valueGreenSwitch: UILabel!
+    @IBOutlet var valueBlueSwitch: UILabel!
+    
+    @IBOutlet var redSwitch: UISlider!
+    @IBOutlet var greenSwitch: UISlider!
+    @IBOutlet var blueSwitch: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        djColorView.layer.cornerRadius = 20
     }
 
-
+    @IBAction func redSwitchScroll() {
+    }
+    @IBAction func greenSwitchScroll() {
+    }
+    @IBAction func blueSwitchScroll() {
+    }
 }
 
