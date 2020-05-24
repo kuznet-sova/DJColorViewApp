e00a02f97af7f2c222f5194996541a9beed74dd6
kuznet.i.sova <kis7425910@gmail.com>
2020-05-24 04:14:05 +0300

    add func mixColors

--- DJColorViewApp/ViewController.swift
+++ DJColorViewApp/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var djColorView: UIView!
     
+    @IBOutlet var redLabel: UIStackView!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
     @IBOutlet var valueRedSwitch: UILabel!
     @IBOutlet var valueGreenSwitch: UILabel!
     @IBOutlet var valueBlueSwitch: UILabel!
@@ -19,17 +23,29 @@ class ViewController: UIViewController {
     @IBOutlet var greenSwitch: UISlider!
     @IBOutlet var blueSwitch: UISlider!
     
+    var redColor: CGFloat = 0.0
+    var greenColor: CGFloat = 0.0
+    var blueColor: CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         djColorView.layer.cornerRadius = 20
     }
-
+    
+    func mixColors() {
+        let mixColor = UIColor.init(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+        djColorView.backgroundColor = mixColor
+    }
+    
     @IBAction func redSwitchScroll() {
         let newValueRedSwitch = Float(Int(redSwitch.value * 100)) / 100
         valueRedSwitch.text = String(newValueRedSwitch)
         
         djColorView.backgroundColor = .red
         djColorView.alpha = CGFloat(newValueRedSwitch)
+        
+        redColor = CGFloat(newValueRedSwitch)
+        mixColors()
     }
     @IBAction func greenSwitchScroll() {
         let newValueGreenSwitch = Float(Int(greenSwitch.value * 100)) / 100
@@ -37,6 +53,9 @@ class ViewController: UIViewController {
         
         djColorView.backgroundColor = .green
         djColorView.alpha = CGFloat(newValueGreenSwitch)
+        
+        greenColor = CGFloat(newValueGreenSwitch)
+        mixColors()
     }
     @IBAction func blueSwitchScroll() {
         let newValueBlueSwitch = Float(Int(blueSwitch.value * 100)) / 100
@@ -44,6 +63,9 @@ class ViewController: UIViewController {
         
         djColorView.backgroundColor = .blue
         djColorView.alpha = CGFloat(newValueBlueSwitch)
+        
+        blueColor = CGFloat(newValueBlueSwitch)
+        mixColors()
     }
 }
 
